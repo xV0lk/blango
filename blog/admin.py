b7faa81a2b37b409bd1cbf2ce9d6ab51df5ebe15
Jorge Rojas <jorge.otto.415@gmail.com>
2022-11-08 03:11:49 +0000

    finish gneric relationship

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from blog.models import Tag, Post
-
-# Register your models here.
-admin.site.register(Tag)
+from blog.models import Tag, Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
   prepopulated_fields = {"slug": ("title",)}
   list_display = ("title", "published_at", "author")
 
+# Register your models here.
+admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,8 @@ class Comment(models.Model):
   content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
   object_id = models.PositiveIntegerField()
   content_object = GenericForeignKey("content_type", "object_id")
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
